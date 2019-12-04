4685db9052151215e0dad4ca77074d7bcebf2de4
Glaveli <g.laveli.p@gmail.com>
2019-12-04 16:00:44 -0300

    upload

--- src/components/DashBoard/LinearProgress/index.js
+++ src/components/DashBoard/LinearProgress/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-
+import DataApi from '../../../services/DataApi';
 
 const Progressbar = styled.div`
 position: absolute;
@@ -17,30 +17,42 @@ animation: progressAnimation 2.5s ease-in-out forwards;
 require('./style.css');
 
 export default class LinearProgressBar extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
-            width: 95,
+            totaoItens: 0,
+            dataResponse: [],
+            width: 100,
         }
+
+
     }
 
 
+    async componentDidMount() {
+        const dataVar = await DataApi.get('dashboards');
+        this.setState({ dataResponse: dataVar.data.recordset, totaoItens: dataVar.data.recordset[0].total })
+
+    }
+
     render() {
+        console.log(this.state.dataResponse);
+
         return (
 
             <div className="linearProgressBar">
                 <div className="container">
-                    <div className="skills">
-
-                        <span className="name">Teste</span>
-
-                        <div className="percent">
-                            <Progressbar width={this.state.width}></Progressbar>
-                            <span className="value">95%</span>
-                        </div>
-                    </div>
-
-
+                    {
+                        this.state.dataResponse.map((item, i) => (
+                            <div className="skills" key={i}>
+                                <span className="name">{item.dataName}</span>
+                                <div className="percent">
+                                    <Progressbar width={(item.total * 100) / this.state.totaoItens}></Progressbar>
+                                    <span className="value">{(item.total * 100) / this.state.totaoItens}%</span>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
 
--- src/pages/DashPage/index.js
+++ src/pages/DashPage/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import CircularProgressBar from '../../components/DashBoard/CircularProgress';
 import LinearProgressBar from '../../components/DashBoard/LinearProgress';
-import DataApi from '../../services/DataApi';
+
 
 export default class DashPage extends Component {
     constructor() {
@@ -11,19 +11,11 @@ export default class DashPage extends Component {
         }
     }
 
-    async componentDidMount() {
-        let response = await DataApi.get('/dashboards');
-        console.log(response);
-
-
-    }
-
     render() {
         return (
+
             <div>
-
                 <LinearProgressBar />
-
             </div>
         )
     }
